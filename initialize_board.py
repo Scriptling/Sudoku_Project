dcacb515df9baacd1ae16726c643dd0e3a66c3e1
전예슬 <jeon-yeseul@jeon-yeseul-ui-MacBookPro.local>
2024-10-28 13:24:08 +0900

    Creat Sudoku_project

---
+++ initialize_board.py
@@ -0,0 +1,5 @@
+
+def initialize_board():
+    return [[0] * 9 for _ in range(9)]
+
+    
---
+++ solve_sudoku.py
@@ -0,0 +1,47 @@
+
+
+def solve_sudoku(board):
+    # 빈값, 아직 안 푼 곳을 찾고
+    empty_loc = find_empty_location(board)
+    if not empty_loc: # not None = True
+        return True  # 모든 빈칸을 다 채운 경우, 끝
+
+    # 빈 위치
+    row, col = empty_loc
+
+    for num in range(1, 10): # 빈값에 1~9를 다 넣어보면서, 유효한 값을 찾는다.
+        if is_valid(board, row, col, num):
+            board[row][col] = num # 유효하면, 대입!
+
+            # 위 규칙에 의거, 값을 채워 넣었는데, 결국 스도쿠를 해결하지 못했다면,
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0  # 실패한 경우, 백트래킹
+
+    return False  # 이 경로로는 해결 불가
+
+# 주어진 스도쿠 퍼즐을 해결하는 함수 호출
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+
+print("초기 스도쿠 퍼즐:")
+print_board(board)
+print("\n해결된 스도쿠 퍼즐:")
+
+if solve_sudoku(board):
+    print_board(board)
+else:
+    print("해결할 수 없는 스도쿠 퍼즐입니다.")
+
+    
